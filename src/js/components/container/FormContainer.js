f1ddd7f72bf2f22a59b3e045b2cb0bfd7a3c7d24
source-code-smith <herman@sourcecodesmith.com>
2018-09-05 01:56:55 +0200

    Using dotenv to get access to pseudo environment variables

--- src/js/components/container/FormContainer.js
+++ src/js/components/container/FormContainer.js
@@ -16,7 +16,7 @@ class FormContainer extends Component {
     const { seo_title } = this.state;
     return (
         <div>
-            <h1>nothing important</h1>
+            <h1>{process.env.SEASON}</h1>
             <form id="article-form">
                 <Input
                     text="SEO title"
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,16 @@
+const webpack = require('webpack');
+const dotenv = require('dotenv');
 const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+// call dotenv and it will return an Object with a parsed key 
+const env = dotenv.config().parsed;
+
+// reduce it to a nice object, the same as before
+const envKeys = Object.keys(env).reduce((prev, next) => {
+    prev[`process.env.${next}`] = JSON.stringify(env[next]);
+    return prev;
+}, {});
+
 module.exports = {
   module: {
     rules: [
@@ -23,6 +35,7 @@ module.exports = {
     new HtmlWebPackPlugin({
       template: "./src/index.html",
       filename: "./index.html"
-    })
+    }),
+    new webpack.DefinePlugin(envKeys)
   ]
 };
