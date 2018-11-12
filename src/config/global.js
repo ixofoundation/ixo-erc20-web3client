b752ebb9b6d4fbe9158f7844aaf6e73d153cb49e
source-code-smith <herman@sourcecodesmith.com>
2018-11-12 13:08:09 +0200

    post truffle migrate

--- src/config/global.js
+++ src/config/global.js
@@ -2,17 +2,17 @@ const erc20Abi = require('./IxoERC20Token.abi.json');
 const projectWalletAbi = require('./ProjectWalletRegistry.abi.json');
 const environments = [{
 		network: 'ropsten',
-		ixoERC20TokenContract: '0x827a41c26784e0f51081e6d26687bff9c1c667e6',
-		projectWalletRegistryContract: '0xfe45b990a1dd890adfac13b0a9c77758cc83a862',
-		authContract: '0x9b79db40c5cec3ccb7401dc1e21eb5c65e7033c9',
-		owner: '0xb4B59C3aCFeB9AFd9398C88B2f6f003cBf29B553'
+		ixoERC20TokenContract: '0xd5250c5c3574476a754619f66ede275e042af3fb',
+		projectWalletRegistryContract: '0xcd155cb5faa1e1505192a769c7549a48dd1834e6',
+		authContract: '0xaa6b6ac3464b71ee9a1036936f3accb7300149b2',
+		owner: '0x647CD1829Ad0FF896640FCd3a29cF6Af0dE10A83'
 	},
 	{
 		network: 'private',
-		tokenContract: '0x827a41c26784e0f51081e6d26687bff9c1c667e6',
-		projectWalletRegistryContract: '0xfe45b990a1dd890adfac13b0a9c77758cc83a862',
-		authContract: '0x9b79db40c5cec3ccb7401dc1e21eb5c65e7033c9',
-		owner: '0xb4B59C3aCFeB9AFd9398C88B2f6f003cBf29B553'
+		ixoERC20TokenContract: '0xdcc9aaf02565239319cff15b287ca35d3e3ed32e',
+		projectWalletRegistryContract: '0x9459b4effe8372c8d8d724fd8b2ba74ed4f461e7',
+		authContract: '0x73ecaaf61d041438ab36cb24f71c868cc03a173b',
+		owner: '0x3E6a6940d6DAdC10820e7f7735E343A3160b0374'
 	}
 ];
 
